9441c4742cc026a568aa0b99f0115afd62c3784d
Alonso Isidoro Román <alonsoir@gmail.com>
2024-04-04 12:21:22 +0200

    *** Added a spark agent *** Added a pandas agent

---
+++ main-spark-python-generator-code.py
@@ -0,0 +1,32 @@
+from dotenv import load_dotenv
+from langchain_experimental.agents.agent_toolkits import create_spark_dataframe_agent
+from langchain_openai import OpenAI
+from pyspark.sql import SparkSession
+
+
+def main():
+    print("hello from spark world!!!")
+    # Run this command to know if there is a java pid using this 15002 port.
+    # lsof -iTCP -sTCP:LISTEN -n -P | grep 15002
+    spark = SparkSession.builder.remote("sc://localhost:15002").getOrCreate()
+    # spark = SparkSession.builder.getOrCreate()
+    csv_file_path = "titanic.csv"
+    df = spark.read.csv(csv_file_path, header=True, inferSchema=True)
+    df.show()
+    agent = create_spark_dataframe_agent(llm=OpenAI(temperature=0), df=df, verbose=True)
+    agent.run("how many rows are there?")
+    agent.run("how many people have more than 3 siblings")
+    agent.run("whats the square root of the average age?")
+    agent.run("Crea un fichero parquet a partir del fichero titanic.csv")
+    agent.run(
+        """
+    who bought the most expensive ticket?
+    You can find all supported function types in https://spark.apache.org/docs/latest/api/python/reference/pyspark.sql/dataframe
+    """
+    )
+    spark.stop()
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    main()
